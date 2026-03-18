57f789e99b7f2793607a6e64b25e85e5a066a9e8
ssscvlnk <pstartceva@yandex.ru>
2026-03-18 14:21:22 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/startseva/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/startseva/unscramblegame/ui_model/GameViewModel.kt
@@ -2,6 +2,7 @@ package com.startseva.unscramblegame.ui_model
 
 import androidx.lifecycle.ViewModel
 import com.startseva.unscramblegame.data.GameUiState
+import com.startseva.unscramblegame.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,10 +10,31 @@ import kotlinx.coroutines.flow.asStateFlow
 class GameViewModel : ViewModel(){
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
     init {
         resetGame()
     }
     fun resetGame() {
-
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambledWord = pickRandomWordAndShuffle()
+        )
+    }
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
     }
 }
